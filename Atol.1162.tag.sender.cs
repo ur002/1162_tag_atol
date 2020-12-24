e26ea388d893fa746aaa244a517e0c6d68fc6a21
Kirill D <dudorov.kv@gmail.com>
2020-12-24 17:47:56 +0300

    Update Atol.1162.tag.sender.cs

--- Atol.1162.tag.sender.cs
+++ Atol.1162.tag.sender.cs
@@ -1,7 +1,52 @@
-# формирование тэга 1162, кода маркировки, для отправки в ОФД. 
+// формирование тэга 1162, кода маркировки, для отправки в ОФД. 
+ private static  byte[] BYTES = new byte[128];
+
 string group = "444D";
 string gtin  = "02900000475830";
 string serial = "MdEfx:Xp6YFd7";
+
+public static byte[] parseHex(String aHexString)
+{
+    char[] src = aHexString.Replace("\n", "").Replace(" ", "").ToUpper().Replace("0X", "").Replace("-", "").ToCharArray();
+    byte[] dst = new byte[src.Length / 2];
+    int si = 0;
+
+    for (int di = 0; di < dst.Length; ++di)
+    {
+        byte high = BYTES[src[si++] & 127];
+        byte low = BYTES[src[si++] & 127];
+        dst[di] = (byte)((high << 4) + (low & 255));
+    }
+
+    return dst;
+}
+
+
+public static String toHexString(byte[] aBytes, int aOffset, int aLength)
+{
+    if (aLength == -1)
+        aLength = aBytes.Length;
+    char[] dst = new char[aLength * 2];
+    int di = 0;
+
+    for (int si = aOffset; si < aOffset + aLength; ++si)
+    {
+        byte b = aBytes[si];
+        dst[di++] = CHARS_TABLES[(b & 240) >> 4];
+        dst[di++] = CHARS_TABLES[b & 15];
+    }
+
+    return new String(dst);
+}
+
+private void SendCode()
+{
+for (int i = 0; i < 10; ++i)
+        {
+            BYTES[48 + i] = (byte)i;
+            BYTES[65 + i] = (byte)(10 + i);
+            BYTES[97 + i] = (byte)(10 + i);
+        }
 
 string str = group.ToUpper() +
              Convert.ToString(long.Parse(gtin), 16).ToUpper().PadLeft(12, '0') +
@@ -25,6 +70,7 @@ Fptr.registration()
 Fptr.setParam(Fptr.LIBFPTR_PARAM_PAYMENT_TYPE, 0) 
 Fptr.closeReceipt()  
 
+}
  
                          
                          
